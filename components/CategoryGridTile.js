8ad26066714af2286b2ec42226bba4a6a0c802e6
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2025-10-10 15:48:26 +0430

    category grid tile component added

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,16 @@
+import { Pressable, View } from "react-native";
+
+
+function CategoryGridTitle({title, color}){
+    return(
+        <View>
+            <Pressable>
+                <View>
+                    <Text>{title}</Text>
+                </View>
+            </Pressable>
+        </View>
+    )
+}
+
+export default CategoryGridTitle;
