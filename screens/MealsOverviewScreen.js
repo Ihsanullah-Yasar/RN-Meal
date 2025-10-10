2efed117d7b6754c06cdc07ccb3dd8071ac39e5b
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2025-10-10 23:48:38 +0430

    meals overview screen added

---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function MealsOverviewScreen() {
+  return (
+    <View>
+      <Text>Meals overview screen</Text>
+    </View>
+  );
+}
+
+export default MealsOverviewScreen;
