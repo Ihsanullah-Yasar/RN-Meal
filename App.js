d822defa47ec61d921310c1177e475b5777fd0ac
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2025-10-12 15:21:20 +0430

    meal details screen added to navigation

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { StyleSheet } from "react-native";
 import CategoryScreen from "./screens/Category";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+import MealDetailScreen from "./screens/MealDetailScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -34,6 +34,14 @@ export default function App() {
           <Stack.Screen
             name="mealsOverview"
             component={MealsOverviewScreen}
+            // options={({ route, navigation }) => {
+            //   const catId = route.params.categoryId;
+            //   return { title: catId };
+            // }}
+          />
+          <Stack.Screen
+            name="mealDetails"
+            component={MealDetailScreen}
             // options={({ route, navigation }) => {
             //   const catId = route.params.categoryId;
             //   return { title: catId };
